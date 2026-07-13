7bfc6d89e7c7860ee4f81c3bd39820bf6a38e564
Yehuda Hason <yehudaxt@gmail.com>
2026-07-13 19:35:48 +0300

    working on menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,31 @@
 "use client";
 import Image from "next/image";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Chart from "./components/Chart";
 import HourlyChart from "./components/HourlyChart";
 import type { Rates } from "./types";
 import { useQuery } from "@tanstack/react-query";
 import { compareRate } from "./fetchMethods/compareRates";
 import { getHourlyData } from "./fetchMethods/getHourlyData";
-import { CURRENCIES } from "@/public/images/flags/map";
+import { CURRENCIES } from "@/public/frankfurter_currencies";
+import { POPULAR_CURRENCIES } from "@/public/popularCurrencies";
+import { getAllcodes } from "./fetchMethods/getAllCodes";
 export default function Home() {
   const [rate, setrate] = useState<Rates>({ quotes: "ILS", base: "USD" });
-
-  const currencies = Object.keys(CURRENCIES);
-
-  const [query, setQuery] = useState("");
+  const mergeObject = { ...CURRENCIES, ...POPULAR_CURRENCIES };
+  const AllCurrencies = Object.keys(mergeObject);
+  const popularCurrencies = Object.keys(POPULAR_CURRENCIES);
+  const othersCurrencies = Object.keys(CURRENCIES);
+  const [query, setQuery] = useState("USD");
   const [open, setOpen] = useState(false);
 
-  const filtered = currencies.filter((c) =>
+  const items = othersCurrencies.filter((c) =>
     c.toLowerCase().includes(query.toLowerCase()),
   );
-  const country = CURRENCIES[rate.base].flag; // "us"
-  const country2 = CURRENCIES[rate.quotes].flag; // "us"
+
+  const country = mergeObject[rate.base].flag;
+  const country2 = mergeObject[rate.quotes].flag;
   const flagUrl = `https://flagcdn.com/w40/${country}.png`;
   const flagUrl2 = `https://flagcdn.com/w40/${country2}.png`;
   const {
@@ -42,6 +46,9 @@ export default function Home() {
     queryFn: () => getHourlyData(rate),
   });
 
+  useEffect(() => {
+    getAllcodes();
+  }, []);
   return (
     <main className="bg-[#171717] min-h-screen flex flex-col items-center w-full mx-auto">
       <div className="flex items-center gap-2 h-fit overflow-hidden">
@@ -49,7 +56,23 @@ export default function Home() {
         <Image src={flagUrl2} width={40} height={25} alt="state" />
       </div>
 
-      <div className="relative w-72">
+      <button className="flex justify-center items-center px-3 py-2 bg-neutral-800 rounded-lg border border-neutral-600">
+        {" "}
+        <div className="flex h-6 w-6 items-center justify-center overflow-hidden rounded-full">
+          <img
+            src={`https://flagcdn.com/${mergeObject[query]?.flag}.svg`}
+            alt={mergeObject[query]?.name}
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <span className="w-12 text-sm font-bold text-white">{query}</span>
+        <img
+          src="/images/icon-chevron-down.svg"
+          alt=""
+          className={`${open ? "rotate-180" : ""} w-4 h-4 transition`}
+        />
+      </button>
+      <div className="relative w-72 p-2 bg-[#1b1d24]  rounded-xl">
         <input
           value={query}
           onChange={(e) => {
@@ -62,25 +85,28 @@ export default function Home() {
         />
 
         {open && (
-          <ul
-            className="absolute z-10 mt-1 max-h-60 w-full  overflow-auto rounded-b-xl  text-yellow-100  flex flex-col 
-          
-          scrollbar  overflow-y-auto border border-zinc-700 bg-[#1b1d24] shadow-xl"
-          >
-            {filtered.map((code) => (
+          <ul className="absolute z-10 mt-0 max-h-80 w-full overflow-y-auto rounded-b-xl bg-[#1b1d24] text-yellow-100 shadow-xl scrollbar left-0">
+            <li className="uppercase flex justify-between items-center font-medium pt-2 px-4 text-gray-400">
+              <span className="text-preset-5-medium">Popular</span>
+              <span className="text-preset-5-medium">
+                {popularCurrencies.length}
+              </span>
+            </li>
+            {popularCurrencies.map((code) => (
               <li
                 key={code}
                 onClick={() => {
                   setQuery(code);
                   setOpen(false);
                 }}
-                className="cursor-pointer px-2 py-4 hover:bg-gray-800 border-b border-b-gray-600"
+                tabIndex={1}
+                className="cursor-pointer border-b border-gray-600 px-2 py-3 hover:bg-gray-800"
               >
                 <div className="flex items-center gap-4">
                   <div className="flex h-6 w-6 items-center justify-center overflow-hidden rounded-full">
                     <img
-                      src={`https://flagcdn.com/${CURRENCIES[code].flag}.svg`}
-                      alt={CURRENCIES[code].name}
+                      src={`https://flagcdn.com/${mergeObject[code].flag}.svg`}
+                      alt={mergeObject[code].name}
                       className="h-full w-full object-cover"
                     />
                   </div>
@@ -90,7 +116,42 @@ export default function Home() {
                   </span>
 
                   <span className="truncate text-sm text-zinc-400">
-                    {CURRENCIES[code].name}
+                    {mergeObject[code].name}
+                  </span>
+                </div>
+              </li>
+            ))}
+            <li className="flex justify-between  uppercase font-medium pt-2 px-3 text-gray-400">
+              <span className="text-preset-5-medium">Others Currencies </span>
+              <span className="text-preset-5-medium">
+                {othersCurrencies.length}
+              </span>
+            </li>
+            {items.map((code) => (
+              <li
+                key={code}
+                onClick={() => {
+                  setQuery(code);
+                  setOpen(false);
+                }}
+                tabIndex={1}
+                className="cursor-pointer border-b border-gray-600 px-2 py-3 hover:bg-gray-800"
+              >
+                <div className="flex items-center gap-4">
+                  <div className="flex h-6 w-6 items-center justify-center overflow-hidden rounded-full">
+                    <img
+                      src={`https://flagcdn.com/${mergeObject[code].flag}.svg`}
+                      alt={mergeObject[code].name}
+                      className="h-full w-full object-cover"
+                    />
+                  </div>
+
+                  <span className="w-12 text-sm font-bold text-white">
+                    {code}
+                  </span>
+
+                  <span className="truncate text-sm text-zinc-400">
+                    {mergeObject[code].name}
                   </span>
                 </div>
               </li>
@@ -107,9 +168,9 @@ export default function Home() {
           0.8530 • MAY 14 16:00 CET
         </div>
       </div>
-      <div className="bg-[#171717] rounded-3xl border h-fit mx-auto max-w-[768px]  w-full border-zinc-800  p-0 shadow-xl text-amber-200">
+      <div className="bg-[#171717] rounded-3xl border h-fit mx-auto max-w-3xl  w-full border-zinc-800  p-0 shadow-xl text-amber-200">
         {ratesLoading ? (
-          <span className="flex animate-spin h-[100px] flex-col items-center-safe justify-center">
+          <span className="flex animate-spin h-25 flex-col items-center-safe justify-center">
             {" "}
             <Image src="/spinner.png" alt="loading" width={80} height={80} />
           </span>
@@ -119,7 +180,7 @@ export default function Home() {
         {ratesError ? ratesError.message : ""}
 
         {hourlyLoading ? (
-          <span className="flex animate-spin h-[100px] flex-col items-center-safe justify-center">
+          <span className="flex animate-spin h-25 flex-col items-center-safe justify-center">
             <Image src="/spinner.png" alt="" height={80} width={80} />
           </span>
         ) : (
---
+++ public/popularCurrencies.ts
@@ -0,0 +1,11 @@
+export const POPULAR_CURRENCIES: Record<
+  string,
+  {
+    flag: string;
+    name: string;
+  }
+> = {
+  USD: { flag: "us", name: "United States Dollar" },
+  EUR: { flag: "eu", name: "Euro" },
+  GBP: { flag: "gb", name: "British Pound" },
+};
